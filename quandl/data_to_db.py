ab4f55b9cf034f6cd4ff2a21f8c809cc9b13b410
Gyra <sun.handing@outlook.com>
2018-09-22 15:38:19 +0100

    connect to mysql

--- quandl/data_to_db.py
+++ quandl/data_to_db.py
@@ -4,7 +4,7 @@
 This is the ORM connecting MySQL
 '''
 
-from sqlalchemy import Column, String, create_engine
+from sqlalchemy import Column, String, DateTime, Float, create_engine, func
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import numpy
@@ -18,3 +18,10 @@ class FinanceData(Base):
     __table_name__ = 'finance_data'
 
     # table structure
+    data_date = Column(DateTime, default=func.now(), primary_key=True)
+    us_gdp = Column(Float(10,6), default=0.0, nullable=True)
+    # initial db connection
+    engine = create_engine('mysql+mysqlconnector:/root:password@localhost:3306/personal_web')
+
+    # create DBSession 
+    DBSession = sessionmaker(bind=engine)
